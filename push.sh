44eb201839a9e44ea01bd8d051616533e0a1abe1
guotao <guotao@firewinggames.com>
2019-03-14 18:55:29 +0800

    update push shell

--- push.sh
+++ push.sh
@@ -1,1 +1,3 @@
- rsync -avzr --exclude="push.sh"  --exclude="package-lock.json" --exclude="config.js" --exclude="app.js" --exclude="node_modules" --exclude=".git"  ./ qqgo:/data/explorer/
+rsync -avzr --exclude="push.sh"  --exclude="package-lock.json" --exclude="config.js" --exclude="app.js" --exclude="node_modules" --exclude=".git"  ./ nnserver:/data/explorer-1/
+ssh nnserver "pm2 restart app"
+
